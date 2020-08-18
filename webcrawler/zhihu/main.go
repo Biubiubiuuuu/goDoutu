5f4f12bb8b4d3232bc78235aff7b328bfb326a21
高超 <gaochao@gaochaodeMacBook-Pro.local>
2020-08-19 00:41:58 +0800

    百度AL 图片识别接入

--- webcrawler/zhihu/main.go
+++ webcrawler/zhihu/main.go
@@ -1,27 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"math"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
-	"github.com/Biubiubiuuuu/goDoutu/goDoutu/webcrawler/zhihu/tools"
 	"github.com/beevik/etree"
+	"github.com/google/uuid"
+	"github.com/qiniu/api.v7/v7/auth/qbox"
+	"github.com/qiniu/api.v7/v7/storage"
 )
 
 // 获取图片Channel
 var ImageDataChannel = make(chan []string, 100)
 
 const (
-	Url      = "https://www.zhihu.com/api/v4/questions/%v/answers?include=data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,relevant_info,question,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,is_labeled,is_recognized,paid_info,paid_info_content;data[*].mark_infos[*].url;data[*].author.follower_count,badge[*].topics&offset=%d&limit=%d&sort_by=default&platform=desktop"
-	Female   = 1
-	Male     = 2
-	Limit    = 5
-	Question = "310564833"
+	AccessKey    = "VKYaoHZ9no66HILp2XmBMl4RwkvZNLX6F67ek3Qd"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         // 七牛云 access_key
+	SecretKey    = "OhvUGMTl45_DykHfhjBMIpd3IKl9g_Qqae2PaXWI"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         // 七牛云 secret_key
+	Bucketname   = "godoutu"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          // 七牛云空间名称
+	NQYBasicUrl  = "http://qezg1i20h.hn-bkt.clouddn.com/"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             // 七牛云外链url
+	MacPath      = "/Users/gaochao/LTWorks/goDoutu/webcrawler/zhihu/basic/image/"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     // 本机下载地址
+	Url          = "https://www.zhihu.com/api/v4/questions/%v/answers?include=data[*].is_normal,admin_closed_comment,reward_info,is_collapsed,annotation_action,annotation_detail,collapse_reason,is_sticky,collapsed_by,suggest_edit,comment_count,can_comment,content,editable_content,voteup_count,reshipment_settings,comment_permission,created_time,updated_time,review_info,relevant_info,question,excerpt,relationship.is_authorized,is_author,voting,is_thanked,is_nothelp,is_labeled,is_recognized,paid_info,paid_info_content;data[*].mark_infos[*].url;data[*].author.follower_count,badge[*].topics&offset=%d&limit=%d&sort_by=default&platform=desktop" // 知乎问题api地址
+	Limit        = 5                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  // 知乎问题返回问题数 最大20条
+	Question     = "310564833"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        // 知乎问题ID
+	ClientID     = "iHCdpZwXdG7TXb5jld7MzvTa"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         // 百度API client_id
+	ClientSecret = "xK6R7zHLLhaTonRzNTTBkwhoLFjVkETr"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 // 百度api client_secret
 )
 
 // 知乎问题回答返回结构体
@@ -137,11 +149,32 @@ type ResponseData struct {
 	} `json:"paging"`
 }
 
+// 百度AL api 授权返回结构体
+type BaiDuALTokenResponse struct {
+	RefreshToken     string `json:"refresh_token"`
+	ExpiresIn        int    `json:"expires_in"`
+	SessionKey       string `json:"session_key"`
+	AccessToken      string `json:"access_token"`
+	Scope            string `json:"scope"`
+	SessionSecret    string `json:"session_secret"`
+	ErrorDescription string `json:"error_description"`
+}
+
+// 百度AL 图片文字识别返回结构体
+type BaiDuALPictureIdentifyResponse struct {
+	LogID          int64 `json:"log_id"`
+	WordsResultNum int   `json:"words_result_num"`
+	WordsResult    []struct {
+		Words string `json:"words"`
+	} `json:"words_result"`
+	ErrorMsg string `json:"error_msg"`
+}
+
 func main() {
 	go DownloadImgToNiuqiyun()
 	// 问题ID 跳转页 页大小
 	url := fmt.Sprintf(Url, Question, 0, Limit)
-	response := Get(url)
+	response := HttpGet(url)
 	var data ResponseData
 	if err := json.Unmarshal([]byte(response), &data); err != nil {
 		panic(err)
@@ -205,9 +238,11 @@ func DownloadImgToNiuqiyun() {
 		for _, v := range url {
 			if v != "" {
 				if aa == 1 {
-					filePath := tools.Download(v)
+					filePath := Download(v)
 					if filePath != "" {
-						tools.Upload(filePath)
+						_, nqyUrl := Upload(filePath)
+						word := BaiDuALPictureIdentify(nqyUrl)
+						fmt.Println(word)
 					}
 				}
 			}
@@ -219,17 +254,120 @@ func DownloadImgToNiuqiyun() {
 
 // 循环请求获取图片
 func GetResponseData(url string) (data ResponseData) {
-	response := Get(url)
+	response := HttpGet(url)
 	if err := json.Unmarshal([]byte(response), &data); err != nil {
 		panic(err)
 	}
 	return
 }
 
+// 七牛云获取token
+func Token() string {
+	putPolicy := storage.PutPolicy{
+		Scope: Bucketname,
+	}
+	mac := qbox.NewMac(AccessKey, SecretKey)
+	return putPolicy.UploadToken(mac)
+}
+
+// 七牛云上传图片
+func Upload(imgUrl string) (err error, url string) {
+	cfg := storage.Config{
+		Zone:          &storage.ZoneHuanan,
+		UseHTTPS:      false,
+		UseCdnDomains: false,
+	}
+	formUploader := storage.NewFormUploader(&cfg)
+	ret := storage.PutRet{}
+	upToken := Token()
+	uuidKey := uuid.New()
+	str := strings.Split(".", imgUrl)
+	key := uuidKey.String() + "." + str[len(str)-1]
+	if err := formUploader.PutFile(context.Background(), &ret, upToken, key, imgUrl, nil); err != nil {
+		return err, ""
+	}
+	return nil, NQYBasicUrl + ret.Key
+}
+
+// 百度API获取授权
+func BaiDuALToken() string {
+	tokenResStr := HttpGet(fmt.Sprintf("https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=%v&client_secret=%v", ClientID, ClientSecret))
+	var res BaiDuALTokenResponse
+	if err := json.Unmarshal([]byte(tokenResStr), &res); err != nil {
+		panic(err)
+	}
+	if res.AccessToken != "" {
+		return res.AccessToken
+	}
+	panic(res.ErrorDescription)
+}
+
+// 百度API文字识别
+func BaiDuALPictureIdentify(imageUrl string) string {
+	access_token := BaiDuALToken()
+	reqUrl := fmt.Sprintf("https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic?access_token=%v", access_token)
+	postData := fmt.Sprintf("url=%v", imageUrl)
+	resJson := HttpPost(reqUrl, postData)
+	var res BaiDuALPictureIdentifyResponse
+	if err := json.Unmarshal([]byte(resJson), &res); err != nil {
+		panic(err)
+	}
+	if len(res.WordsResult) > 0 {
+
+		var str []string
+		for _, v := range res.WordsResult {
+			str = append(str, v.Words)
+		}
+		return strings.Join(str, ",")
+	}
+	panic(res.ErrorMsg)
+}
+
+// 下载图片到本地
+func Download(url string) string {
+	str := strings.Split(url, ".")
+	fileName := uuid.New().String() + "." + strings.Replace(str[len(str)-1], "?source=1940ef5c", "", -1)
+	res, err := http.Get(url)
+	if err != nil {
+		return ""
+	}
+	defer res.Body.Close()
+	reader := bufio.NewReaderSize(res.Body, 32*1024)
+	time := time.Now().Format("20060102")
+	path := MacPath + time
+	if !IsExist(path) {
+		CreateDir(path)
+	}
+	path = path + "/" + fileName
+	file, err := os.Create(path)
+	if err != nil {
+		return ""
+	}
+	writer := bufio.NewWriter(file)
+	io.Copy(writer, reader)
+	return path
+}
+
+// 文件夹创建
+func CreateDir(path string) error {
+	err := os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	os.Chmod(path, os.ModePerm)
+	return nil
+}
+
+// 判断文件夹/文件是否存在
+func IsExist(f string) bool {
+	_, err := os.Stat(f)
+	return err == nil || os.IsExist(err)
+}
+
 // 发送GET请求
 // url：         请求地址
 // response：    请求返回的内容
-func Get(url string) string {
+func HttpGet(url string) string {
 	// 超时时间：5秒
 	client := &http.Client{Timeout: 5 * time.Second}
 	resp, err := client.Get(url)
@@ -249,4 +387,21 @@ func Get(url string) string {
 		}
 	}
 	return result.String()
+}
+
+// 发送POST请求
+// url：         请求地址
+// response：    请求返回的内容
+func HttpPost(url string, postData string) string {
+	resp, err := http.Post(url, "application/x-www-form-urlencoded", strings.NewReader(postData))
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return string(body)
 }
